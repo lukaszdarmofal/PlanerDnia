b16fae717f0db4c503ba1f03dd907185c9e0329d
Łukasz Darmofał <lukasz.darmofal@cyfroweszkoly.pl>
2025-03-24 11:18:02 +0100

    Created DeleteButton functionality and finished the app

--- PlanerDnia/MainWindow.axaml.cs
+++ PlanerDnia/MainWindow.axaml.cs
@@ -28,24 +28,61 @@ public partial class MainWindow : Window
                 newTask.Name = "Zadanie bez nazwy";
             }
         
-            var taskName = new TextBlock { Text = newTask.Name, HorizontalAlignment = HorizontalAlignment.Center };
-            var taskCategory = new ComboBox { HorizontalAlignment = HorizontalAlignment.Center };
+            var taskName = new TextBlock
+            {
+                Text = newTask.Name, 
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            
+            var taskCategory = new ComboBox
+            {
+                HorizontalAlignment = HorizontalAlignment.Center 
+            };
             taskCategory.Items.Add(new TextBlock { Text = "Ważne" });
             taskCategory.Items.Add(new TextBlock { Text = "Priorytetowe" });
             taskCategory.Items.Add(new TextBlock { Text = "Poboczne" });
             taskCategory.SelectedIndex = newTask.CategoryIndex;
-            var taskFinished = new CheckBox { IsChecked = newTask.IsFinished, Content = "Zakończono", HorizontalAlignment = HorizontalAlignment.Center };
-            var taskDelete = new Button { Content = "Usuń", HorizontalAlignment = HorizontalAlignment.Center};
-            taskDelete.Click += DeleteTask;
             
-            var taskElement = new StackPanel { Width = 180, Height = 130 };
+            var taskFinished = new CheckBox
+            {
+                IsChecked = newTask.IsFinished, 
+                Content = "Zakończono", 
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            
+            var taskDelete = new Button
+            {
+                Content = "Usuń", 
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            
+            taskDelete.Click += (sender, args) =>
+            {
+                var button = (Button)sender;
+                var stackPanel = (StackPanel)button.Parent;
+                var border = (Border)stackPanel.Parent;
+                
+                Panel_TaskGrid.Children.Remove(border);
+            };
+            
+            var taskElement = new StackPanel
+            {
+                Width = 180, 
+                Height = 130
+            };
             taskElement.Margin = new Thickness(10);
             taskElement.Children.Add(taskName);
             taskElement.Children.Add(taskCategory);
             taskElement.Children.Add(taskFinished);
             taskElement.Children.Add(taskDelete);
             
-            var taskBorder = new Border { Width = 190 , Height = 150, BorderBrush = Brushes.Salmon , Background = Brushes.Salmon };
+            var taskBorder = new Border
+            {
+                Width = 190 , 
+                Height = 150, 
+                BorderBrush = Brushes.Salmon , 
+                Background = Brushes.Salmon
+            };
             taskBorder.Margin = new Thickness(5);
             taskBorder.BorderThickness = new Thickness(3);
             taskBorder.CornerRadius = new CornerRadius(3);
@@ -55,9 +92,5 @@ public partial class MainWindow : Window
 
         }
     }
-
-    private void DeleteTask(object sender, RoutedEventArgs e)
-    {
-        
-    }
+    
 }
