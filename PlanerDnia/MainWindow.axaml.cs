4b809dba73878f1283b82e1fdca4d6f100657301
Łukasz Darmofał <lukasz.darmofal@cyfroweszkoly.pl>
2025-03-24 10:45:45 +0100

    Created cool aestethics

--- PlanerDnia/MainWindow.axaml.cs
+++ PlanerDnia/MainWindow.axaml.cs
@@ -1,7 +1,10 @@
 using System;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Layout;
+using Avalonia.LogicalTree;
+using Avalonia.Media;
 
 namespace PlanerDnia;
 
@@ -18,27 +21,43 @@ public partial class MainWindow : Window
     {
         Task newTask = new(TextBox_TaskName.Text, ComboBox_Category?.SelectedItem.ToString(), false, ComboBox_Category.SelectedIndex);
 
-        if (newTask.Name == null)
+        if (Panel_TaskGrid.Children.Count < 8)
         {
-            newTask.Name = "Zadanie bez nazwy";
+            if (newTask.Name == null)
+            {
+                newTask.Name = "Zadanie bez nazwy";
+            }
+        
+            var taskName = new TextBlock { Text = newTask.Name, HorizontalAlignment = HorizontalAlignment.Center };
+            var taskCategory = new ComboBox { HorizontalAlignment = HorizontalAlignment.Center };
+            taskCategory.Items.Add(new TextBlock { Text = "Ważne" });
+            taskCategory.Items.Add(new TextBlock { Text = "Priorytetowe" });
+            taskCategory.Items.Add(new TextBlock { Text = "Poboczne" });
+            taskCategory.SelectedIndex = newTask.CategoryIndex;
+            var taskFinished = new CheckBox { IsChecked = newTask.IsFinished, Content = "Zakończono", HorizontalAlignment = HorizontalAlignment.Center };
+            var taskDelete = new Button { Content = "Usuń", HorizontalAlignment = HorizontalAlignment.Center};
+            taskDelete.Click += DeleteTask;
+            
+            var taskElement = new StackPanel { Width = 180, Height = 130 };
+            taskElement.Margin = new Thickness(10);
+            taskElement.Children.Add(taskName);
+            taskElement.Children.Add(taskCategory);
+            taskElement.Children.Add(taskFinished);
+            taskElement.Children.Add(taskDelete);
+            
+            var taskBorder = new Border { Width = 190 , Height = 150, BorderBrush = Brushes.Salmon , Background = Brushes.Salmon };
+            taskBorder.Margin = new Thickness(5);
+            taskBorder.BorderThickness = new Thickness(3);
+            taskBorder.CornerRadius = new CornerRadius(3);
+            taskBorder.Child = taskElement;
+            
+            Panel_TaskGrid.Children.Add(taskBorder);
+
         }
-        
-        var taskName = new TextBlock { Text = newTask.Name };
-        var taskCategory = new ComboBox { };
-        taskCategory.Items.Add(new TextBlock { Text = "Ważne" });
-        taskCategory.Items.Add(new TextBlock { Text = "Priorytetowe" });
-        taskCategory.Items.Add(new TextBlock { Text = "Poboczne" });
-        taskCategory.SelectedIndex = newTask.CategoryIndex;
-        var taskFinished = new CheckBox { IsChecked = newTask.IsFinished, Content = "Zakończono" };
-        var taskDelete = new Button { Content = "Usuń" };
+    }
 
-        var taskElement = new StackPanel { Width = 200, Height = 150 };
-        taskElement.Children.Add(taskName);
-        taskElement.Children.Add(taskCategory);
-        taskElement.Children.Add(taskFinished);
-        taskElement.Children.Add(taskDelete);
+    private void DeleteTask(object sender, RoutedEventArgs e)
+    {
         
-        Panel_TaskGrid.Children.Add(taskElement);
-
     }
 }
